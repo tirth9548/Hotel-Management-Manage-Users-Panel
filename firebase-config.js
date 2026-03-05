30691188f73904c7fed934fca591d18037981d98
tirth9548 <tirth9548@gmail.com>
2026-03-06 00:06:10 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // Firebase Configuration for Hotel Grand Plaza
-// Database URL: https://hotel-grand-plaza-db-default-rtdb.firebaseio.com/
-// Project ID: hotel-grand-plaza-db
+// Database URL: https://hotel-grand-plaza-db-c62d3-default-rtdb.firebaseio.com/
+// Project ID: hotel-grand-plaza-db-c62d3
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
@@ -9,14 +9,14 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyALFXaA521YaigaJ9J2Vx270nkvbctgyzI",
-    authDomain: "hotel-grand-plaza-db.firebaseapp.com",
-    databaseURL: "https://hotel-grand-plaza-db-default-rtdb.firebaseio.com",
-    projectId: "hotel-grand-plaza-db",
-    storageBucket: "hotel-grand-plaza-db.firebasestorage.app",
-    messagingSenderId: "318765519849",
-    appId: "1:318765519849:web:6675205f6e49a4228fb178",
-    measurementId: "G-S3LF56444M"
+    apiKey: "AIzaSyDCLmSvngqB1QK10vEms__9N3AizPU6X1c",
+    authDomain: "hotel-grand-plaza-db-c62d3.firebaseapp.com",
+    databaseURL: "https://hotel-grand-plaza-db-c62d3-default-rtdb.firebaseio.com",
+    projectId: "hotel-grand-plaza-db-c62d3",
+    storageBucket: "hotel-grand-plaza-db-c62d3.firebasestorage.app",
+    messagingSenderId: "746768995375",
+    appId: "1:746768995375:web:9b60f5afb9f9fdc503a2da",
+    measurementId: "G-RPMWZ74FEE"
 };
 
 // Initialize Firebase
@@ -45,3 +45,4 @@ window.firebaseServices = {
 };
 
 console.log('Firebase initialized successfully!');
+
